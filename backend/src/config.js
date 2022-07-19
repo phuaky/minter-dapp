e613501075646fb73c32be44c51ddbe727e0ccdb
phuaky <championpky@gmail.com>
2022-07-19 21:07:17 +0800

    update contract

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,10 +58,10 @@ const MINT_PRICE = 0.001 // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH
 const TOKENS_PER_MINT = 10 // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = '2022-07-19T11:30:48+00:00' // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = '2022-07-17T11:30:48+00:00' // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = '2022-07-17T11:30:48+00:00' // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000 // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = '0xFC11Ba54b1570fEBD6b03d75286Ecb80BEFBd44b' // Address that will receive the royalty
 const BASE_URI = null // only update if you want to manually set the base uri
