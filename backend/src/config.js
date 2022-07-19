869a2ef03c1744dbcdc92f606861968202f4ebc6
phuaky <championpky@gmail.com>
2022-07-19 20:51:54 +0800

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,90 +1,93 @@
-require('dotenv').config();
-const basePath = process.cwd();
-const fs = require("fs");
-const { MODE } = require(`${basePath}/constants/blend_mode.js`);
-const { NETWORK } = require(`${basePath}/constants/network.js`);
+require('dotenv').config()
+const basePath = process.cwd()
+const fs = require('fs')
+const { MODE } = require(`${basePath}/constants/blend_mode.js`)
+const { NETWORK } = require(`${basePath}/constants/network.js`)
 
-const network = NETWORK.eth;
+const network = NETWORK.eth
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
+const namePrefix = 'eyebirdz.xyz'
+const description = 'Starting eye testes'
+const baseUri = 'ipfs://NewUriToReplace' // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 10,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: 'Background' },
+      { name: 'Eyeball' },
+      { name: 'Eye color' },
+      { name: 'Iris' },
+      { name: 'Shine' },
+      { name: 'Bottom lid' },
+      { name: 'Top lid' },
     ],
   },
-];
+]
 
-const shuffleLayerConfigurations = true;
+const shuffleLayerConfigurations = true
 
-const debugLogs = false;
+const debugLogs = false
 
 const format = {
   width: 512,
   height: 512,
   smoothing: false,
-};
+}
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
-};
+  // Replace with your website or remove this line if you do not have one.
+}
 
 // NFTPort Info
 
 // ** REQUIRED **
-const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
-const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
+const AUTH = process.env.NFTPORT_API_KEY // Set this in the .env file to prevent exposing your API key when pushing to Github
+const LIMIT = 2 // Your API key rate limit
+const CHAIN = 'rinkeby' // only rinkeby, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
-const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const CONTRACT_NAME = 'eyebirdz.xyz'
+const CONTRACT_SYMBOL = 'EBZ'
+const METADATA_UPDATABLE = true // set to false if you don't want to allow metadata updates after minting
+const OWNER_ADDRESS = '0xFC11Ba54b1570fEBD6b03d75286Ecb80BEFBd44b'
+const TREASURY_ADDRESS = '0xFC11Ba54b1570fEBD6b03d75286Ecb80BEFBd44b'
+const MAX_SUPPLY = 5000 // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001 // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10 // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = '2022-07-19T11:30:48+00:00' // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
-const BASE_URI = null; // only update if you want to manually set the base uri
-const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_MINT_START_DATE = null // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = 1000 // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = '0xFC11Ba54b1570fEBD6b03d75286Ecb80BEFBd44b' // Address that will receive the royalty
+const BASE_URI = null // only update if you want to manually set the base uri
+const PREREVEAL_TOKEN_URI = null // only update if you want to manually set the prereveal token uri
+const PRESALE_WHITELISTED_ADDRESSES = [
+  '0xFC11Ba54b1570fEBD6b03d75286Ecb80BEFBd44b',
+] // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = 'YOUR CONTRACT ADDRESS' // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
-const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC = true // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC_TITLE = CONTRACT_NAME // Replace with what you want the generic titles to say if you want it to be different from the contract name.
+const GENERIC_DESCRIPTION = 'Wait for eye' // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE =
+  'https://ipfs.io/ipfs/bafkreiagbna6e2hb3lnrxpv7465tdpq2em5ymflgq7mrxycvtu3cwqyqem' // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
   const rawContractData = fs.readFileSync(
     `${basePath}/build/contract/_contract.json`
-  );
-  const contractData = JSON.parse(rawContractData);
-  if (contractData.response === "OK") {
-    CONTRACT_ADDRESS = contractData.contract_address;
+  )
+  const contractData = JSON.parse(rawContractData)
+  if (contractData.response === 'OK') {
+    CONTRACT_ADDRESS = contractData.contract_address
   }
 } catch (error) {
   // Do nothing, falling back to manual contract address
@@ -92,67 +95,67 @@ try {
 // END NFTPort Info
 
 const solanaMetadata = {
-  symbol: "YC",
+  symbol: 'YC',
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: 'https://www.youtube.com/c/hashlipsnft',
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: '7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC',
       share: 100,
     },
   ],
-};
+}
 
 const gif = {
   export: false,
   repeat: 0,
   quality: 100,
   delay: 500,
-};
+}
 
 const text = {
   only: false,
-  color: "#ffffff",
+  color: '#ffffff',
   size: 20,
   xGap: 40,
   yGap: 40,
-  align: "left",
-  baseline: "top",
-  weight: "regular",
-  family: "Courier",
-  spacer: " => ",
-};
+  align: 'left',
+  baseline: 'top',
+  weight: 'regular',
+  family: 'Courier',
+  spacer: ' => ',
+}
 
 const pixelFormat = {
   ratio: 2 / 128,
-};
+}
 
 const background = {
   generate: true,
-  brightness: "80%",
+  brightness: '80%',
   static: false,
-  default: "#000000",
-};
+  default: '#000000',
+}
 
-const rarityDelimiter = "#";
+const rarityDelimiter = '#'
 
-const uniqueDnaTorrance = 10000;
+const uniqueDnaTorrance = 10000
 
 const preview = {
   thumbPerRow: 5,
   thumbWidth: 50,
   imageRatio: format.height / format.width,
-  imageName: "preview.png",
-};
+  imageName: 'preview.png',
+}
 
 const preview_gif = {
   numberOfImages: 5,
-  order: "ASC", // ASC, DESC, MIXED
+  order: 'ASC', // ASC, DESC, MIXED
   repeat: 0,
   quality: 100,
   delay: 500,
-  imageName: "preview.gif",
-};
+  imageName: 'preview.gif',
+}
 
 module.exports = {
   format,
@@ -195,5 +198,5 @@ module.exports = {
   PUBLIC_MINT_START_DATE,
   BASE_URI,
   PREREVEAL_TOKEN_URI,
-  PRESALE_WHITELISTED_ADDRESSES
-};
+  PRESALE_WHITELISTED_ADDRESSES,
+}
